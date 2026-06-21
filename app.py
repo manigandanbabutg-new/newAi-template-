a7ad76a811a7eb2e8472afbf8f014b203d440b51
manigandanbabutg-new <manigandanbabu.tg@gmail.com>
2026-06-21 17:49:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import joblib
 
+model = joblib.load("model.pkl")
+
+def predict(user_data):
+
+    result = model.predict([user_data])
+
+    if result[0] == 1:
+        return "Recommended"
+    else:
+        return "Not Recommended"
+
+# Example Input
+
+user = [24, 1, 1]
+
+output = predict(user)
+
+print(output)
